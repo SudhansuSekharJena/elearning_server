d6c8b2b2b72cb13fd967305c585dfd00ab9d17c6
SudhansuSekharJena <87571856+SudhansuSekharJena@users.noreply.github.com>
2025-12-01 09:56:07 +0530

    Simplify CORS origin configuration

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ const port = process.env.PORT || 3000
 // It means the data we will send from frontend will be in the form of json.
 app.use(express.json())
 app.use(cors({
-   origin: [
-    "http://localhost:5173",
-    "https://elearning-frontend-blush.vercel.app"
-  ],
+   origin:"https://elearning-frontend-blush.vercel.app",
   methods:"GET, POST, PUT, DELETE",
   credentials: true,
 }))
